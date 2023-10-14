e097bfdebd65e6986e32fbd26b4fb62cf9fb0671
dstieglitz <dstieglitz@stainlesscode.com>
2023-10-14 14:19:42 -0700

    working timing (minimal testing)

--- papermill_kubernetes_job_progress/__init__.py
+++ papermill_kubernetes_job_progress/__init__.py
@@ -58,6 +58,7 @@ class KubernetesJobProgressEngine(NBClientEngine):
     # messages will not be sent
     #
     loop = asyncio.new_event_loop()
+    nats_error = False
     done = False
 
     try:
@@ -83,21 +84,53 @@ class KubernetesJobProgressEngine(NBClientEngine):
         pass
 
     @classmethod
+    async def error_cb(cls, e):
+        print(e)
+        cls.nats_error = True
+
+    @classmethod
+    async def disconnected_cb(cls, e):
+        print(e)
+
+    @classmethod
+    async def reconnected_cb(cls, e):
+        print(e)
+        print("CONNECTED");
+
+    @classmethod
     async def nats_connect(cls):
         print(f"nats_connect")
         if cls.nats_debug:
             print(f"Connecting to {cls.nats_url}...")
-        await cls.nc.connect(cls.nats_url,
-                             user=cls.nats_user,
-                             password=cls.nats_password,
-                             verbose=True)
+        if not cls.nc.is_connected:
+            await cls.nc.connect(cls.nats_url,
+                                 user=cls.nats_user,
+                                 password=cls.nats_password,
+                                 verbose=True,
+                                 error_cb=cls.error_cb,
+                                 disconnected_cb=cls.disconnected_cb,
+                                 reconnected_cb=cls.reconnected_cb,
+                                 max_reconnect_attempts=3)
         if cls.nats_debug:
             print(f"Connected to {cls.nats_url}")
 
     @classmethod
+    async def nats_disconnect(cls):
+        if cls.nats_debug:
+            print('Disconnecting from NATS...')
+        if cls.nc.is_connected:
+            await cls.nc.close()
+            await asyncio.sleep(1, loop=cls.loop)
+
+    @classmethod
     async def nats_send(cls, cell_index, cell_count, start_time, end_time, duration, progress, type='progress'):
+        if cls.nats_error:
+            return
         print(f"nats_send {cell_index}")
         await cls.nats_connect()
+        if cls.nats_error:
+            print("Couldn't connect")
+            return
         msg = {
             'notebook_id': cls.notebook_id,
             'timestamp': str(datetime.utcnow().isoformat()),
@@ -110,7 +143,6 @@ class KubernetesJobProgressEngine(NBClientEngine):
             'progress': progress
         }
         await cls.nc.publish(cls.subject, bytes(json.dumps(msg, default=str), 'utf-8'))
-        await cls.nc.drain()
         if cls.nats_debug:
             print(f"sent {json.dumps(msg, default=str)}")
 
@@ -120,6 +152,8 @@ class KubernetesJobProgressEngine(NBClientEngine):
 
     @classmethod
     async def stop_loop(cls):
+        print("stop_loop")
+        await cls.nats_disconnect()
         tasks = asyncio.all_tasks(loop=cls.loop)
         # print(tasks)
         cls.loop.stop()
@@ -147,7 +181,6 @@ class KubernetesJobProgressEngine(NBClientEngine):
 
         def patched_notebook_complete(**kwargs):
             print(f"Patched notebook_complete")
-            asyncio.run_coroutine_threadsafe(cls.stop_loop(), loop=cls.loop)
             orig_notebook_complete(**kwargs)
 
         def patched_cell_exception(cell, cell_index=None, **kwargs):
@@ -172,24 +205,26 @@ class KubernetesJobProgressEngine(NBClientEngine):
         def patched_cell_complete(cell, cell_index, **kwargs):
             print(f"Patched cell complete {cell_index}")
             orig_cell_complete(cell, cell_index, **kwargs)
-
-            ratio_progress = (cell_index + 1) / len(nb_man.nb.cells)
-            # progress is a value between 0 and 100
-            progress = round(ratio_progress * 100)
-            print(f"Progress is {progress}%")
-            cell_end_timestamp = datetime.utcnow()
-            duration = cell_end_timestamp - cls.cell_start_timestamp
-            print(f"Duration is  {duration}")
-            progress_future = asyncio.run_coroutine_threadsafe(
-                cls.nats_send(
-                    cell_index,
-                    len(nb_man.nb.cells),
-                    cls.cell_start_timestamp,
-                    cell_end_timestamp,
-                    duration,
-                    progress),
-                loop=cls.loop)
-            print(progress_future)
+            if cell.metadata.papermill['status'] == "failed":
+                print("FAILURE DETECTED")
+            else:
+                ratio_progress = (cell_index + 1) / len(nb_man.nb.cells)
+                # progress is a value between 0 and 100
+                progress = round(ratio_progress * 100)
+                print(f"Progress is {progress}%")
+                cell_end_timestamp = datetime.utcnow()
+                duration = cell_end_timestamp - cls.cell_start_timestamp
+                print(f"Duration is  {duration}")
+                progress_future = asyncio.run_coroutine_threadsafe(
+                    cls.nats_send(
+                        cell_index,
+                        len(nb_man.nb.cells),
+                        cls.cell_start_timestamp,
+                        cell_end_timestamp,
+                        duration,
+                        progress),
+                    loop=cls.loop)
+                print(progress_future)
 
         nb_man.cell_complete = patched_cell_complete
         nb_man.cell_start = patched_cell_start
@@ -201,7 +236,7 @@ class KubernetesJobProgressEngine(NBClientEngine):
         #     asyncio.ensure_future(cls.nc.close())
 
         print("Starting progress thread")
-        threading.Thread(target=cls.run_loop, daemon=False).start()
+        threading.Thread(target=cls.run_loop, daemon=True).start()
 
         print(f"Calling super...")
         super().execute_managed_notebook(nb_man,
@@ -213,7 +248,9 @@ class KubernetesJobProgressEngine(NBClientEngine):
                                          execution_timeout=None,
                                          **kwargs)
 
-        while len(asyncio.all_tasks(loop=cls.loop)) > 0:
+        asyncio.run_coroutine_threadsafe(cls.stop_loop(), loop=cls.loop)
+        while not cls.nats_error and len(asyncio.all_tasks(loop=cls.loop)) > 0:
             print("sleeping")
             print(asyncio.all_tasks(loop=cls.loop))
             sleep(2)
+
